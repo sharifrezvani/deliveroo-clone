b7ff56d03102bd45b7c119a67adb01c8130b48b6
Sharif Rezvani <sharifrezvanii@gmail.com>
2022-12-10 23:15:39 +0330

    Fifth Milestone: Ready for Redux

--- components/DishRow.js
+++ components/DishRow.js
@@ -1,11 +1,77 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    Image
+} from 'react-native'
+import
+React,
+{ useState }
+    from 'react'
+import { urlFor } from '../sanity'
+import {
+    MinusCircleIcon,
+    PlusCircleIcon
+} from 'react-native-heroicons/solid'
 
-const DishRow = () => {
+const DishRow = ({
+    id,
+    name,
+    description,
+    price,
+    image
+}) => {
+    const [isPressed, setIsPressed] = useState(false)
     return (
-        <View>
-            <Text>DishRow</Text>
-        </View>
+        <>
+            <TouchableOpacity
+                onPress={() => setIsPressed(!isPressed)}
+                className={`bg-white border-y p-4 border-gray-200 ${isPressed && 'border-b-0'} `}
+            >
+                <View className='flex-row'>
+                    <View className='flex-1 pr-2'>
+                        <Text className='text-lg mb-1'>{name}</Text>
+                        <Text className='text-gray-400'>{description}</Text>
+                        <Text className='text-gray-400 mt-2'>
+                            £ {price.toFixed(2)}
+                        </Text>
+                    </View>
+                    <View>
+                        <Image
+                            style={{
+                                borderWidth: 1,
+                                borderColor: '#F3F3F4'
+                            }}
+                            source={{
+                                uri: urlFor(image).url()
+                            }}
+                            className='h-20 aspect-square bg-gray-300 p-4 rounded-md'
+                        />
+                    </View>
+                </View>
+            </TouchableOpacity>
+            {isPressed && (
+                <View className='bg-white px-4'>
+                    <View className='flex-row items-center space-x-2 pb-3'>
+                        <TouchableOpacity>
+                            <MinusCircleIcon
+                                // color={items.length > 0 ? "#00CCBB" : "gray"}
+                                color='#00CCBB'
+                                size={30}
+                            />
+                        </TouchableOpacity>
+                        <Text>0</Text>
+                        <TouchableOpacity>
+                            <PlusCircleIcon
+                                // color={items.length > 0 ? "#00CCBB" : "gray"}
+                                color='#00CCBB'
+                                size={30}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            )}
+        </>
     )
 }
 
